c3476a4bb433c84c37cb1a6f5e939b1fbbd9b958
RifaiMahdi <rifaimahdy35@gmail.com>
2026-01-28 09:29:10 +0700

    update

---
+++ belajarphp/belajar.php
@@ -0,0 +1,25 @@
+<?php
+    $nama_lengkap = "fauziah nur islami";
+    $jenis_kelamin = "P";
+    $umur ="16 tahun";
+
+    echo "Nama: $nama_lengkap<br>";
+    echo "Jenis Kelamin: $jenis_kelamin<br>";
+    echo "Umur: $umur tahun<br>";
+
+    echo "<br><br>";
+
+    $namabarang ="blush on";
+    $harga = "50000";
+    $stok = "12";
+
+    echo "Nama: $namabarang<br>";
+    echo "harga: $harga<br>";
+    echo "Umur: $stok<br>";
+    echo "saya membeli $namabarang seharga $harga";
+
+    echo "<br><br>";
+
+    $judul = "belajar make up natural"
+    echo 'judul artikel: '.$judul;
+
