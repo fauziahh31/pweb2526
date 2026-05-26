3d9a92514d341f360e9c1ef26cbafaf02bedb0f4
fauziahh31 <fnurislami31@gmail.com>
2026-05-26 10:42:33 +0700

    up

--- crudwv/proses_login.php
+++ crudwv/proses_login.php
@@ -1,47 +1,27 @@
 <?php
 session_start();
 include "koneksi.php";
- 
-if(isset($_POST['login'])){
- 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
- 
-    $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username'");
-$cek = mysqli_num_rows($query);
 
-if($cek > 0) {
-    echo "Username ketemu! "; // Ini buat tes
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username'");
+
+if(mysqli_num_rows($query) > 0){
     $data = mysqli_fetch_assoc($query);
-    // Cek password manual
-    if($password == $data['password']) {
-        echo "Password cocok!";
-        // ... (lanjutkan session kamu di sini)
-    } else {
-        echo "Password di database: " . $data['password']; // Biar kamu tahu isi database
-    }
-} else {
-    echo "Username tidak ada di database!";
-}
-die();
-        $data = mysqli_fetch_assoc($query);
- 
+
+    // Kita bandingkan password apa adanya
+    if($password == $data['password']){
         $_SESSION['user_id']  = $data['id'];
         $_SESSION['username'] = $data['username'];
         $_SESSION['role']     = $data['role'];
- 
+
         header("Location: index.php");
         exit;
- 
-    }else{
- 
-        echo "
-        <script>
-        alert('Username atau password salah!');
-        window.location='login.php';
-        </script>
-        ";
- 
+    } else {
+        echo "<script>alert('Password salah!'); window.location='login.php';</script>";
     }
+} else {
+    echo "<script>alert('Username tidak terdaftar!'); window.location='login.php';</script>";
 }
 ?>
